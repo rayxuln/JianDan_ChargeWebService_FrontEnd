f3e09a820e41a1b86b9dcdffe931706fe47e294c
BuildTools <unconfigured@null.spigotmc.org>
2020-10-07 19:54:27 +0800

    Complete charge sub-page

--- src/components/subpages/ChargeSubpage.vue
+++ src/components/subpages/ChargeSubpage.vue
@@ -1,7 +1,25 @@
 <template>
     <div class="white-box">
+        <a-modal v-model="charge_result_window_visibility" title="收费单">
+            <template slot="footer">
+                <a-button key="submit" type="primary" @click="onPrintButtonClicked"> 打印 </a-button>
+            </template>
+
+            <div id="print-box" style="width: 100%;height: 100%;">
+                <div style="float: left;"> 房号：{{ charge_result_window_data_source.house_id }} </div>
+                <div style="float: left;margin-left: 35px;"> 缴纳月份：{{ charge_result_window_data_source.charge_date_interval }} </div>
+                <div style="float: right;"> 业主：{{ charge_result_window_data_source.house_owner_name }} </div>
+
+                <a-table :columns="charge_result_window_charges_table_columns" :data-source="charge_result_window_data_source.charges" bordered :pagination='false'>
+                </a-table>
+
+                <div style="float: left;"> 收费日期：{{ charge_result_window_data_source.charge_date }} </div>
+                <div style="float: right;"> 员工号：{{ charge_result_window_data_source.staff_id }} </div>
+            </div>
+        </a-modal>
+
         <div id="title"> 收费 </div>
-        <a-form-model ref="charge_form" :model="charge_form" :label-col="{span:4}" :wrapper-col="{span:14}">
+        <a-form-model ref="charge_form" :model="charge_form" :rules='charge_form_rules' :label-col="{span:4}" :wrapper-col="{span:14}">
             <a-form-model-item label="房号" prop="house_id">
                 <a-select v-model="charge_form.house_id" placeholder="请选择房号" :loading="!has_houses_loaded">
                     <a-select-option v-for="house in houses" :key="house.house_id" :value="house.house_id">
@@ -20,7 +38,11 @@
             <div v-for="(charge, index) in charge_form.multy_charges" :key="index" class="charge-box">
                 <div> 序号: {{ index }} </div>
 
-                <a-form-model-item label="收费类型" prop="type">
+                <a-form-model-item label="收费类型" :prop="'multy_charges.' + index + '.type'" :rules="{
+                    required: true,
+                    message: '请选择收费类型',
+                    trigger: 'change',
+                }">
                     <a-select v-model="charge.type" placeholder="请选择收费类型">
                         <a-select-option value="物业费">
                             物业费
@@ -37,7 +59,11 @@
                     </a-select>
                 </a-form-model-item>
 
-                <a-form-model-item label="数量" prop="number">
+                <a-form-model-item label="数量" :prop="'multy_charges.' + index + '.number'" :rules="{
+                    required: true,
+                    message: '请输入收费数量',
+                    trigger: 'blur',
+                }">
                     <a-input-number v-model="charge.number" :step="0.01" />
                 </a-form-model-item>
 
@@ -51,9 +77,14 @@
             添加一个收费条目
         </a-button>
 
-        <a-button type="primary" style="margin-top: 15px;text-align: center;">
+        <a-button type="primary" style="margin-top: 15px;" @click="onChargeButtonClicked" :loading="is_charging">
             <a-icon type="dollar" />
             确认收费
+        </a-button>
+
+        <a-button style="margin-top: 15px;margin-left: 15px;" @click="onResetButtonClicked">
+            <a-icon type="reload" />
+            重置收费
         </a-button>
     </div>
 </template>
@@ -61,6 +92,32 @@
 <script>
 import locale from 'ant-design-vue/es/date-picker/locale/zh_CN';
 import util from '../../util';
+
+const renderNumberColumnContent = (value, row) => {
+    const obj = {
+        children: value,
+        attrs: {},
+    };
+    // console.log(value + ':' + JSON.stringify(row) + ':' + index)
+    if(row.key === '合计')
+    {
+        obj.attrs.colSpan = 0;
+    }
+    return obj;
+};
+
+const renderTypeColumnContent = (value, row) => {
+    const obj = {
+        children: value,
+        attrs: {},
+    };
+    // console.log(value + ':' + JSON.stringify(row) + ':' + index)
+    if(row.key === '合计')
+    {
+        obj.attrs.colSpan = 2;
+    }
+    return obj;
+};
 
 export default {
     name: "ChargeSubpage",
@@ -81,22 +138,53 @@ export default {
             houses: [
                 {
                     house_id: 0,
-                    house_owner_name: '望天吧',
-                },
-                {
-                    house_id: 1,
-                    house_owner_name: '望天吧',
-                },
-                {
-                    house_id: 2,
-                    house_owner_name: '帕普斯',
-                },
-                {
-                    house_id: 3,
-                    house_owner_name: '衫斯',
+                    house_owner_name: '加载中...',
                 },
             ],
-            has_houses_loaded: false
+            has_houses_loaded: false,
+            is_charging: false,
+
+            charge_result_window_visibility: false,
+            charge_result_window_data_source: {},
+            charge_result_window_charges_table_columns: [
+                {
+                    title: '序号',
+                    dataIndex: 'key',
+                },
+                {
+                    title: '收费类型',
+                    dataIndex: 'type',
+                    customRender: renderTypeColumnContent,
+                },
+                {
+                    title: '数量',
+                    dataIndex: 'number',
+                    customRender: renderNumberColumnContent,
+                },
+                {
+                    title: '金额',
+                    dataIndex: 'charge',
+                },
+            ],
+            charge_form_rules: {
+                charge_ym_start: [
+                    {required: true, message: '请选择收费开始日期', trigger: 'change'},
+                    {message: '开始日期不能晚于结束日期！', trigger: 'change', validator(rule) {
+                        if(rule.that.charge_form.charge_ym_end == '') return true
+                        return (new Date(rule.that.charge_form.charge_ym_start)) <= (new Date(rule.that.charge_form.charge_ym_end));
+                    }, that: this}
+                ],
+                charge_ym_end: [
+                    {required: true, message: '请选择收费结束日期', trigger: 'change'},
+                    {message: '结束日期不能早于开始日期！', trigger: 'change', validator(rule) {
+                        if(rule.that.charge_form.charge_ym_start == '') return true
+                        return (new Date(rule.that.charge_form.charge_ym_start)) <= (new Date(rule.that.charge_form.charge_ym_end));
+                    }, that: this}
+                ],
+                house_id: [
+                    {required: true, message: '请选择要收费的房号', trigger: 'change'},
+                ]
+            }
         }
     },
     created: function(){
@@ -137,6 +225,77 @@ export default {
                 }
                 that.has_houses_loaded = true
             })})
+        },
+        onChargeButtonClicked(){
+            var that = this
+
+            this.$refs.charge_form.validate(valid => {
+                if(valid)
+                {
+                    if(that.charge_form.multy_charges.length === 0)
+                    {
+                        util.message.error("请至少添加一项收费条目！")
+                    }else{
+                        that.letsCharge()
+                    }
+                }else{
+                    return false
+                }
+            })
+        },
+        letsCharge(){
+            var that = this
+            that.is_charging = true
+            var token = util.getCookie("token")
+            let body_data = {...that.charge_form}
+            body_data.charge_ym_start = util.dateToString(new Date(body_data.charge_ym_start))
+            body_data.charge_ym_end = util.dateToString(new Date(body_data.charge_ym_end))
+            fetch(
+                "/api/charge?token="+token,
+                {
+                    method: "POST",
+                    body: JSON.stringify(body_data),
+                    headers: new Headers({
+                        'content-type': 'application/json;charset=utf-8'
+                    })
+                }
+            ).then(function(res){res.json().then(function(res){
+                if(res.code === 0)
+                {
+                    util.message.success("收费成功！")
+                    that.onResetButtonClicked()
+
+                    that.charge_result_window_data_source = {...res.data.charge_info}
+                    that.charge_result_window_data_source.charges.push({
+                        key: '合计',
+                        type: util.fixed2NumberToChineseNumber(res.data.charge_info.charge_sum),
+                        number: 0,
+                        charge: res.data.charge_info.charge_sum,
+                    })
+                    that.charge_result_window_visibility = true
+
+                    // console.log(res.data.charge_info)
+                }else if(res.code === -1)
+                {
+                    util.logoutAndJumpToLoginPage(that)
+                }else{
+                    util.message.error(res.msg)
+                }
+                that.is_charging = false
+            })})
+        },
+        onResetButtonClicked(){
+            let s = this.charge_form.charge_ym_start
+            let e = this.charge_form.charge_ym_end
+
+            this.$refs.charge_form.resetFields()
+            this.charge_form.multy_charges = []
+            this.charge_form.charge_ym_start = s
+            this.charge_form.charge_ym_end = e
+        },
+        onPrintButtonClicked(){
+            util.printJS('print-box', 'html')
+            // console.log('printing?...')
         }
     }
 }
--- src/util.js
+++ src/util.js
@@ -1,8 +1,30 @@
 import { message } from 'ant-design-vue'
+import printJS from 'print-js'
 
+const numberToChineseNumberMap = {
+    '0': '零',
+    '1': '壹',
+    '2': '贰',
+    '3': '叁',
+    '4': '肆',
+    '5': '伍',
+    '6': '陆',
+    '7': '柒',
+    '8': '捌',
+    '9': '玖',
+}
+
+const unitToChineseUnitMap = {
+    0: '拾',
+    1: '佰',
+    2: '仟',
+    3: '万',
+    yi: '亿',
+}
 
 export default {
     message,
+    printJS,
     getCookie: function(cname) {
         var name = cname + "=";
         var ca = document.cookie.split(';');
@@ -41,6 +63,72 @@ export default {
         that.$store.commit("logout")
         this.deleteCookie("token")
         that.$router.push({name: "Login"})
+    },
+    fixed2NumberToChineseNumber(x){
+        if(typeof x === 'number')
+            x = x.toFixed(2)
+        x = '' + x
+        let yuan = x.split('.')[0]
+        let jiao = numberToChineseNumberMap[x[x.length-2]]
+        let fen = numberToChineseNumberMap[x[x.length-1]]
+    
+        let res = ''
+    
+        let cnt = -1
+        let zero_flag = false
+        for(let i=yuan.length-1; i>=0; --i, ++cnt)
+        {
+            let num = numberToChineseNumberMap[yuan[i]] //数字
+            let unit = cnt === -1 ? '元' : unitToChineseUnitMap[cnt%4] //单位
+            if(unit === '万' && cnt%8 === 7) unit = unitToChineseUnitMap.yi
+    
+            if(num === '零'){
+                if(!zero_flag)
+                {
+                    if(cnt%4 !== 3)
+                        res = num + res
+                }
+                zero_flag = true
+            }
+            else zero_flag = false
+            
+            if(!zero_flag)
+                res = (num + unit) + res
+            else{
+                if(cnt%4 === 3)
+                {
+                    res = unit + res
+                }
+            }
+        }
+
+        let tail = ''
+    
+        if(jiao != '零')
+        {
+            tail += jiao + '角'
+        }else{
+            tail += jiao
+        }
+    
+        if(fen != '零')
+        {
+            tail += fen + '分'
+        }else{
+            tail += '整'
+        }
+
+        if(res[res.length-1] !== '元')
+        {
+            res += '元'
+        }
+
+        if(jiao === '零' && fen === '零')
+        {
+            tail = '零角零分'
+        }
+    
+        return res+tail
     }
 }
 
